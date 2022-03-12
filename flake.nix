16e94bad97a53dd93eb39281b4da9045ba8c37a8
Ingo Ruhnke <grumbel@gmail.com>
2022-03-12 22:15:59 +0100

    Replace mk/cmake/ with external/cmake-modules/

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "gitlab:grumbel/cmake-modules";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -18,6 +22,9 @@
             src = nixpkgs.lib.cleanSource ./.;
             nativeBuildInputs = [
               pkgs.cmake
+            ];
+            buildInputs = [
+              tinycmmc.defaultPackage.${system}
             ];
            };
         };
