1bd72d23821696513a0b9cf029e63506253bc5c7
Ingo Ruhnke <grumbel@gmail.com>
2025-11-16 01:38:44 +0100

    Update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Command Line Argument Parser for C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -11,14 +11,13 @@
 
     strutcpp.url = "github:grumbel/strutcpp";
     strutcpp.inputs.nixpkgs.follows = "nixpkgs";
-    strutcpp.inputs.flake-utils.follows = "flake-utils";
     strutcpp.inputs.tinycmmc.follows = "tinycmmc";
   };
 
   outputs = { self, nixpkgs, flake-utils, tinycmmc, strutcpp }:
     tinycmmc.lib.eachSystemWithPkgs (pkgs:
       {
-        packages = flake-utils.lib.flattenTree rec {
+        packages = rec {
           default = argpp;
           argpp = pkgs.callPackage ./argpp.nix {
             strutcpp = strutcpp.packages.${pkgs.buildPlatform.system}.default;
